56e04ba93a0156f0d790c26f455f42709b4cc8ba
Rushikesh Mahajan <rushikesh.mahajan@johnlewis.co.uk>
2025-07-19 13:37:59 +0530

    Changes in batch job

--- src/main/java/com/example/batch/BatchCsvProcessorApplication.java
+++ src/main/java/com/example/batch/BatchCsvProcessorApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BatchCsvProcessorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BatchCsvProcessorApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        System.exit(
+            SpringApplication.exit(
+                SpringApplication.run(BatchCsvProcessorApplication.class, args)
+            )
+        );
+    }
 }
